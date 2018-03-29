8ac3d17575488452a9ac70dd41aff97a116ec17e
unknown <smurfination1@gmail.com>
2018-03-29 10:14:52 +0800

    Update1

--- ServerWithSecurity.java
+++ ServerWithSecurity.java
@@ -22,34 +22,26 @@ public class ServerWithSecurity {
 			connectionSocket = welcomeSocket.accept();
 			fromClient = new DataInputStream(connectionSocket.getInputStream());
 			toClient = new DataOutputStream(connectionSocket.getOutputStream());
-
+			toClient.write(1); //send acknowledgement for ready to get cert.
 			while (!connectionSocket.isClosed()) {
 
-				int packetType = fromClient.readInt();
-
-				// If the packet is for transferring the filename
+				int packetType = fromClient.readInt();// If the packet is for transferring the filename
 				if (packetType == 0) {
-
-					System.out.println("Receiving file...");
-
+					System.out.println("Receiving cert name...");
 					int numBytes = fromClient.readInt();
 					byte [] filename = new byte[numBytes];
 					fromClient.read(filename);
-
-					fileOutputStream = new FileOutputStream("recv/"+new String(filename, 0, numBytes));
+					fileOutputStream = new FileOutputStream("C:/Users/User/Desktop/Server/"+new String(filename, 0, numBytes)); //save to your desktop.
 					bufferedFileOutputStream = new BufferedOutputStream(fileOutputStream);
-
-				// If the packet is for transferring a chunk of the file
-				} else if (packetType == 1) {
-
+				}
+				else if (packetType == 1) {// If the packet is for transferring a chunk of the file
 					int numBytes = fromClient.readInt();
 					byte [] block = new byte[numBytes];
 					fromClient.read(block);
-
 					if (numBytes > 0)
 						bufferedFileOutputStream.write(block, 0, numBytes);
-
-				} else if (packetType == 2) {
+				}
+				else if (packetType == 2) {
 
 					System.out.println("Closing connection...");
 
@@ -66,3 +58,37 @@ public class ServerWithSecurity {
 	}
 
 }
+
+
+/*
+	while (!connectionSocket.isClosed()) {
+
+				int packetType = fromClient.readInt();// If the packet is for transferring the filename
+				if (packetType == 0) {
+					System.out.println("Receiving cert name...");
+					int numBytes = fromClient.readInt();
+					byte [] filename = new byte[numBytes];
+					fromClient.read(filename);
+					fileOutputStream = new FileOutputStream("C:/Users/User/Desktop/Server/"+new String(filename, 0, numBytes)); //save to your desktop.
+					bufferedFileOutputStream = new BufferedOutputStream(fileOutputStream);
+				}
+				else if (packetType == 1) {// If the packet is for transferring a chunk of the file
+					int numBytes = fromClient.readInt();
+					byte [] block = new byte[numBytes];
+					fromClient.read(block);
+					if (numBytes > 0)
+						bufferedFileOutputStream.write(block, 0, numBytes);
+				}
+				else if (packetType == 2) {
+
+					System.out.println("Closing connection...");
+
+					if (bufferedFileOutputStream != null) bufferedFileOutputStream.close();
+					if (bufferedFileOutputStream != null) fileOutputStream.close();
+					fromClient.close();
+					toClient.close();
+					connectionSocket.close();
+				}
+
+			}
+ */
