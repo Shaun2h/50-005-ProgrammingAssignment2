40948f368046ae258cf8448fe96542d053163c36
unknown <smurfination1@gmail.com>
2018-03-31 23:04:14 +0800

    keygen for AES

---
+++ Keygen.java
@@ -0,0 +1,23 @@
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import static jdk.nashorn.internal.runtime.Context.printStackTrace;
+
+public class Keygen {
+    public SecretKey createAES(){
+        try{
+            KeyGenerator kg = KeyGenerator.getInstance("AES");
+            SecureRandom random  = new SecureRandom();
+            kg.init(random);
+            SecretKey key = kg.generateKey(); //generate the AES key.
+            return key;
+        }
+        catch(NoSuchAlgorithmException ex){
+            System.out.println("No Such Algorithm in KeyGen");
+            printStackTrace(ex);
+        }
+
+    }
+}
