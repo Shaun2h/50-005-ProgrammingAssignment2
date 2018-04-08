f71b06f6d3de504031cabd166db700f92a657e04
unknown <smurfination1@gmail.com>
2018-04-08 22:01:12 +0800

    Completed command line interface

--- PoohClient.java
+++ PoohClient.java
@@ -1,23 +1,99 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class PoohClient {
     public static void main(String[] args){
-        ClientWithSecurity client = new ClientWithSecurity("Alice_Cert.crt",4321,"localhost","unencryptedprivatekeyALICE.der");
-        client.start();
-        long timeStarted = System.nanoTime();
-        client.sendplaincert();
-        client.receivecert();
-        client.clean_Streams();
-        client.verify_Certs("BOB");
-        //client.sendWith_ServerPublicKeyEncrypted("rr.txt"); //FOR SENDING VIA RSA ENCRYPTION
-        client.sendFile("rr.txt");
+        String certloc="";
+        String portnum="";
+        String who;
+        String privatekey="";
+        String targetip="";
+        String who_is_that="";
+        boolean do_or_dont=true;
+        try {
+            BufferedReader inputreader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("running using default settings? type \"y\" if this is so. case sensitive");
+            if(inputreader.readLine().equals("y")){
+                certloc = "Alice_Cert.crt";
+                portnum = "4321";
+                privatekey = "unencryptedprivatekeyALICE.der";
+                who_is_that = "BOB";
+                do_or_dont=false;
+            }
+            if (do_or_dont) {
+
+                System.out.println("Please enter your cert file name (Case sensitive, space sensitive, just sensitive in general) type d for default.");
+                certloc = inputreader.readLine();
+                System.out.println("Please enter your port number. type d for default.");
+                portnum = inputreader.readLine();
+                System.out.println("enter private key. type d for default. Keep in mind this system only accepts ALICE and BOB's Cert. this is more of where the private key is located.");
+                privatekey = inputreader.readLine();
+                System.out.println("enter who the opposing party is. type d for default. Actually there isn't a point in typing anything else. it's ALICE by default. anything you else is taken as BOB");
+                who = inputreader.readLine();
+                System.out.println("enter who the opposing party is. type d for default. Actually there isn't a point in typing anything else. it's ALICE by default. anything you else is taken as BOB");
+                targetip = inputreader.readLine();
+                if (certloc.equals("d")) {
+                    certloc = "Alice_Cert.crt";
+                }
+                if (portnum.equals("d")) {
+                    portnum = "4321";
+                }
+                if (privatekey.equals("d")) {
+                    privatekey = "unencryptedprivatekeyALICE.der";
+                }
+
+                if (who.equals("d")) {
+                    who_is_that = "BOB";
+                } else {
+                    who_is_that = "ALICE";
+                }
+            }
+
+            System.out.println("Please enter the file you want sent over. Ensure it is in the same directory");
+            String filename = inputreader.readLine();
+
+            System.out.println("Please enter what type of file sending you are looking for:");
+            System.out.println("1 = send the file plain, after ensuring certificates are correct");
+            System.out.println("2 = send the file encrypted with RSA encryption, after ensuring certificates are correct");
+            System.out.println("3 = send the file with AES encryption, after ensuring certificates are correct, and exchanging keys.");
+            System.out.println("if you enter anything else, the server won't do anything and will exit immediately after verifying certificates.");
+            String type = inputreader.readLine();
+            System.out.println("initialising server....");
+
+            ClientWithSecurity client = new ClientWithSecurity(certloc, Integer.parseInt(portnum), targetip, privatekey);
+            System.out.println("Attempting to do find server.");
+            client.start();
+            long timeStarted = System.nanoTime();
+            client.sendplaincert();
+            client.receivecert();
+            client.clean_Streams();
+            client.verify_Certs(who_is_that);
+
+            if(type.equals("1")){
+                client.sendFile(filename);
+            }
+            if(type.equals("2")){
+                client.sendWith_ServerPublicKeyEncrypted(filename); //FOR SENDING VIA RSA ENCRYPTION
+            }
+            if(type.equals("3")){
+                client.receiveSessionKey();
+                client.clean_Streams();
+                client.send_file_with_AES();
+                client.clean_Streams();
+            }
 
 
-        /*client.receiveSessionKey();
-        client.clean_Streams();
-        client.send_file_with_AES();
-        client.clean_Streams();
-        */
-        long timeTaken = System.nanoTime() - timeStarted;
-        System.out.println("Program took: " + timeTaken/1000000.0 + "ms to run - Client side Timing");
+            long timeTaken = System.nanoTime() - timeStarted;
+            System.out.println("Program took: " + timeTaken / 1000000.0 + "ms to run - Client side Timing");
+        }
+        catch(IOException ex){
+            System.out.println("entire system didn't work.");
+        }
+        catch(NumberFormatException ex){
+            System.out.println("You didn't enter a proper port number...");
+            ex.printStackTrace();
+        }
     }
 }
 
--- ServerPiglet.java
+++ ServerPiglet.java
@@ -1,24 +1,97 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ServerPiglet {
     public static void main(String[] args){
-        ServerWithSecurity server = new ServerWithSecurity("Bob_Cert.crt",4321,"unencryptedprivatekeyBOB.der");
-        server.start();
-        long timeStarted = System.nanoTime();
-        server.receivecert();
-        server.sendplaincert();
-        server.clean_Streams();
-        server.verify_Certs("ALICE");
-        //server.receieve_file_with_SERVER_PrivateKey(); //FOR SENDING VIA RSA ENCRYPTION
-        server.receiveFile(); //receive the file...
+        BufferedReader inputreader = new BufferedReader(new InputStreamReader(System.in));
+        String certloc="";
+        String portnum="";
+        String who;
+        String privatekey="";
+        String targetip="";
+        String who_is_that="";
+        boolean do_or_dont=true;
+        try{
+            System.out.println("running using default settings? type \"y\" if this is so. case sensitive");
+            if(inputreader.readLine().equals("y")){
+                certloc = "Bob_Cert.crt";
+                portnum = "4321";
+                privatekey = "unencryptedprivatekeyBOB.der";
+                who_is_that = "ALICE";
+                do_or_dont=false;
+            }
+            if(do_or_dont) {
+                System.out.println("Please enter your cert file name (Case sensitive, space sensitive, just sensitive in general) type d for default.");
+                certloc = inputreader.readLine();
+                System.out.println("Please enter your port number. type d for default.");
+                portnum = inputreader.readLine();
+                System.out.println("enter private key. type d for default.");
+                privatekey = inputreader.readLine();
+                System.out.println("enter who the opposing party is. type d for default. Actually there isn't a point in typing anything else. it's ALICE by default. anything you else is taken as BOB");
+                who = inputreader.readLine();
+                if (certloc.equals("d")) {
+                    certloc = "Bob_Cert.crt";
+                }
+                if (portnum.equals("d")) {
+                    portnum = "4321";
+                }
+                if (privatekey.equals("d")) {
+                    privatekey = "unencryptedprivatekeyBOB.der";
+                }
+                if (who.equals("d")) {
+                    who_is_that = "ALICE";
+                } else {
+                    who_is_that = "BOB";
+                }
+            }
+            System.out.println("Please enter what type of file sending you are looking for:");
+            System.out.println("1 = send the file plain, after ensuring certificates are correct");
+            System.out.println("2 = send the file encrypted with RSA encryption, after ensuring certificates are correct");
+            System.out.println("3 = send the file with AES encryption, after ensuring certificates are correct, and exchanging keys.");
+            System.out.println("if you enter anything else, the server won't do anything and will exit immediately after verifying certificates.");
+            String type = inputreader.readLine();
+            System.out.println("initialising server....");
+            ServerWithSecurity server = new ServerWithSecurity(certloc,Integer.parseInt(portnum),privatekey);
+            System.out.println("now awaiting connection");
+            server.start(); //await connection.
+
+            //connection established...
+
+            long timeStarted = System.nanoTime(); //start the timer.
+
+            server.receivecert();
+            server.sendplaincert();
+            server.clean_Streams();
+
+            server.verify_Certs(who_is_that);
+            if(type.equals("1")){
+                server.receiveFile();
+            } //receive the file...
+            if(type.equals("2")){
+                server.receieve_file_with_SERVER_PrivateKey();
+            }//FOR SENDING VIA RSA ENCRYPTION
+            if(type.equals("3")){
+                server.shareSessionKey();
+                server.clean_Streams();
+                server.receieve_file_with_AES();
+                server.clean_Streams();
+            }
 
 
-        /*
-        server.shareSessionKey();
-        server.clean_Streams();
-        server.receieve_file_with_AES();
-        server.clean_Streams();
-        */
-        long timeTaken = System.nanoTime() - timeStarted;
-        System.out.println("Program took: " + timeTaken/1000000.0 + "ms to run - Server side Timing");
+            long timeTaken = System.nanoTime() - timeStarted;
+            System.out.println("Program took: " + timeTaken/1000000.0 + "ms to run - Server side Timing");
+        }
+        catch(IOException ex){
+            System.out.println("entire thing failed.");
+            ex.printStackTrace();
+        }
+        catch(NumberFormatException ex){
+            System.out.println("You didn't enter a proper port number...");
+            ex.printStackTrace();
+        }
+
+
     }
 }
 
